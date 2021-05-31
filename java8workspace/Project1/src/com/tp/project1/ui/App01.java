28f2f81082d57c42db6166a3d4e2e4332d37df05
Varshitha Pedapati <pss.varshitha@gmail.com>
2021-05-31 19:00:30 +0530

    Project1

---
+++ java8workspace/Project1/src/com/tp/project1/ui/App01.java
@@ -0,0 +1,11 @@
+package com.tp.project1.ui;
+
+public class App01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+	}
+
+}
